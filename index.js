e73eca1410afc3f082ee5aa7a980874ba3a8d895
Caio <eugeniocaio10@gmail.com>
2020-07-12 13:28:38 -0400

    Promisified lstat

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const fs = require("fs");
 
+const { lsat } = fs.promises;
+
 fs.readdir(process.cwd(), (err, filenames) => {
   if (err) {
     throw new Error(err);
-  } 
+  }
 
-  for (let filename of filenames){
-    fs.lstat(filename, (err,stats )=> {
-      if(err){
+  for (let filename of filenames) {
+    fs.lstat(filename, (err, stats) => {
+      if (err) {
         console.log(err);
-      } 
+      }
 
       console.log(filename, stats.isFile());
-      
     });
   }
 });
